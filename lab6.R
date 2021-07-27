2dd5b7f86c392e5f30dfd529506335f4e7ba6b26
Matias <matias.coronado@usach.cl>
2021-07-26 21:10:17 -0400

    Codigo al 95%

--- lab6.R
+++ lab6.R
@@ -36,7 +36,7 @@ retardos_multi <- function(
 
 #### Procesamiento DATOS A ####
 ## 70% of the sample size
-datos <- datosB
+datos <- datosA
 
 attach(datos)
 #Tiempo de muestreo
@@ -46,9 +46,34 @@ Tiempo=seq(Ts,(length(VFSC))*Ts,Ts)
 formula=VFSC ~ PAM
 set.seed(123)
 
+# Costos mas bajos 0.0625
+# NU: 0 - 0.2
+# Variar de 0.05
+# Gamma: -3 - 2
+# Variar de 1
+
+#### ORIGINAL
 registerDoParallel(cores = 6)
 cost <- 2^seq(-4, 12, 2)
 nu <- seq(0.1, 0.9, 0.4)
+gamma<-2^seq(-4, 12, 2)
+lagsList<-seq(1,5,1)
+
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+# SUJETO A
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+# TEST 1. NU
+registerDoParallel(cores = 7)
+cost <- 2^seq(-4, 12, 2)
+nu <- seq(0.1, 0.2, 0.05)
 gamma<-2^seq(-4, 12, 2)
 lagsList<-seq(1,5,1)
 
@@ -60,9 +85,9 @@ data <- data.frame(PAMn,VFSCn)
 smp_size <- floor(0.50 * nrow(data))
 ## set the seed to make your partition reproducible
 set.seed(123)
-train <- data[1:smp_size,]
+test <- data[1:smp_size,]
 aux <- smp_size+1
-test <- data[aux:nrow(data),]
+train <- data[aux:nrow(data),]
 
 #Se obtienen los mejores modelos
 parms <- expand.grid(lagsList=lagsList, cost = cost, nu = nu, gamma=gamma)
@@ -76,6 +101,7 @@ salida <- (c( foreach(i = 1:nrow(parms),  combine = rbind, .inorder = FALSE) %do
   retDatos=signal.train$folded.signal
   x=subset(retDatos, select = -VFSCn)
   y=retDatos$VFSCn
+  set.seed(123)
   modelo <- e1071::svm(x, y, type = "nu-regression", kernel = "radial", cost = c, nu = n, gamma=g)
   dataframe <- data.frame(PAMn = test$PAMn)
   colnames <- c("PAMn")
@@ -93,12 +119,35 @@ salida <- (c( foreach(i = 1:nrow(parms),  combine = rbind, .inorder = FALSE) %do
   c(l, c, n, g, corr_pred)
 }))
 
-output <- matrix(unlist(salida), ncol = 5, byrow = TRUE)
-# Se ordenan por correlacion.
-mejoresModelos<-output[order(output[,5], decreasing = TRUE),]
-print(mejoresModelos)
+output0 <- matrix(unlist(salida), ncol = 5, byrow = TRUE)
 
-mejoresModelos.DatosB.parte1 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ####### ####### #######  ####### ####### ####### 
 # Para ver el tema de la capacidad de autoregulacion!.
